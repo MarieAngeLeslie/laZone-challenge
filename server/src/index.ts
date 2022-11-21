7f9febaf04124ee96799c06f5a3180eb50d0cf8c
MarieAngeLes <marieangeleslie0305@gmail.com>
2022-11-21 18:06:06 +0100

    feat: environment configuration done succesfully

---
+++ server/src/index.ts
@@ -0,0 +1,55 @@
+import "reflect-metadata";
+import express from "express";
+import cors from "cors";
+import { ApolloServer } from "apollo-server-express";
+
+import { openDBConnection } from "./utils/database";
+import config from "./constants";
+import { createSchema } from "./utils/createSchema";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { userResolver } from "./resolvers/user";
+
+const main = async () => {
+  let retries = Number(config.dbConnectionRetries);
+  const retryTimeout = Number(config.timeoutBeforeRetry);
+
+  while (retries) {
+    try {
+      const conn = await openDBConnection();
+      await conn.synchronize();
+      await conn.runMigrations();
+      break;
+    } catch (error) {
+      retries -= 1;
+      console.log(error);
+      console.log(`retries left: ${retries}`);
+      await new Promise((res) => setTimeout(res, retryTimeout));
+    }
+  }
+
+  const app = express();
+
+  //set up cors with express cors middleware
+  app.use(
+    cors({ origin: [config.frontend_url, config.studio_apollo_graphql_url] })
+  );
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, userResolver],
+      validate: false,
+    }),
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app, cors: false });
+
+  app.listen(config.port, () => {
+    console.log(`server started on port ${config.port}`);
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
---
+++ server/src/types.ts
@@ -0,0 +1,12 @@
+import { InputType, Field } from "type-graphql";
+
+@InputType()
+export class UserInput {
+
+  @Field(() => String)
+  username!: string;
+
+  @Field(() => String)
+  email!: string;
+  
+}
